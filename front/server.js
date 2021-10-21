8c75a1007fe483c5251d964ae00cdfe729555e42
Paul <paulvignon99@gmail.com>
2021-10-21 14:51:42 +0200

    1

---
+++ front/server.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const app = express()
+app.use("/css",express.static("./css"))
+app.use("/images",express.static("./images"))
+app.use("/js",express.static("./js"))
+app.use("/",express.static("./html"))
+app.listen(8080)
