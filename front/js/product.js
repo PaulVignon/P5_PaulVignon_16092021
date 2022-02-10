2f7b03847e207f017e7c808561b0859829b5c488
Paul <paulvignon99@gmail.com>
2022-02-10 09:24:44 +0100

    Changement Get ID

--- front/js/product.js
+++ front/js/product.js
@@ -1,19 +1,32 @@
 let identifiant = "107fb5b75607497b96722bda5b504926";
 
+let templateTitle = document.querySelector("#h1title");
+
+function fetch_id(data) {
+  let clone = document.importNode(templateTitle.content, true);
+  clone.querySelector("#title").textContent = data.name;
+}
+
 //Récuperer l'identifiant
-function
+function getID() {
+  return identifiant;
+}
+
+function renderProduct() {
+  let _idProduct = await getID();
+  for (const product of _idProduct) {
+    let renderedProduct = fetch_id(product);
+    section.appendChild(renderedProduct);
+  }
+}
 
 //Récuperer les données à partir de l'identifiant
-function name(params) {
-    
-}
-//Affichage Rendue à partir des données
-function name(params) {
-    
+function getDataFromId(id) {
+  fetch(`http://localhost:3000/api/products/${id}`);
 }
 
+//Affichage Rendue à partir des données
+function name(params) {}
 
 // Effectuer le rendue à partir de l'identifiant
-function name(params) {
-    
-}
+function name(params) {}
