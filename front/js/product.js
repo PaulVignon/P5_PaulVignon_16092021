8851a2c404df2cd431f75e36fde0426f7197e78a
Paul <paulvignon99@gmail.com>
2022-02-12 10:40:20 +0100

    Selection array api specifique

--- front/js/product.js
+++ front/js/product.js
@@ -1,32 +1,28 @@
-let identifiant = "107fb5b75607497b96722bda5b504926";
-
-let templateTitle = document.querySelector("#h1title");
-
-function fetch_id(data) {
-  let clone = document.importNode(templateTitle.content, true);
-  clone.querySelector("#title").textContent = data.name;
-}
-
 //Récuperer l'identifiant
 function getID() {
-  return identifiant;
-}
-
-function renderProduct() {
-  let _idProduct = await getID();
-  for (const product of _idProduct) {
-    let renderedProduct = fetch_id(product);
-    section.appendChild(renderedProduct);
-  }
+  return new URL(document.location.href).searchParams.get("id");
 }
 
 //Récuperer les données à partir de l'identifiant
 function getDataFromId(id) {
-  fetch(`http://localhost:3000/api/products/${id}`);
+  return fetch(`http://localhost:3000/api/products/${id}`)
+    .then((res) => res.json())
+    .then((promise) => {
+      /*apiData = promise;*/
+      console.log(promise);
+      return promise;
+    });
 }
 
 //Affichage Rendue à partir des données
-function name(params) {}
+function renderProduct(data) {}
 
 // Effectuer le rendue à partir de l'identifiant
-function name(params) {}
+const fetchData = async () => {
+  let identifiant = getID();
+  let data = await getDataFromId(identifiant);
+  console.log(data);
+  renderProduct(data);
+};
+
+fetchData();
