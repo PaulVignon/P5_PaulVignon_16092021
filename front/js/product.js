50ab81f2594b5bde330a8435a0b488b5c9efa425
Paul <paulvignon99@gmail.com>
2022-02-18 10:01:27 +0100

    Affichage Rendue Produit

--- front/js/product.js
+++ front/js/product.js
@@ -15,21 +15,44 @@ function getDataFromId(id) {
 
 //Affichage Rendue à partir des données
 function renderProduct(data) {
-  let imgProduct = `<img src="${data.imageUrl}" alt="${data.altTxt}"></img>`;
+  document.querySelector("#imgPrdct").src = data.imageUrl;
+  document.querySelector("#imgPrdct").alt = data.altTxt;
+  document.querySelector("#title").textContent = data.name;
+  document.querySelector("#price").textContent = data.price;
+  document.querySelector("#description").textContent = data.description;
+  let newOption = document.createElement("option");
+  newOption.textContent = data.colors;
+  newOption.value = "0";
+  document.querySelector("#colors").appendChild(newOption);
+
+  /*let imgProduct = `<img src="${data.imageUrl}" alt="${data.altTxt}"></img>`;
   let nameProduct = `${data.name}`;
   let priceProduct = `${data.price}`;
   let descProduct = `${data.description}`;
   console.log(imgProduct);
   console.log(nameProduct);
   console.log(priceProduct);
-  console.log(descProduct);
+  console.log(descProduct);*/
 }
+
+renderAllProducts();
 // Effectuer le rendue à partir de l'identifiant
 const fetchData = async () => {
   let identifiant = getID();
+  console.log(identifiant);
   let data = await getDataFromId(identifiant);
   console.log(data);
   renderProduct(data);
 };
 
+async function renderAllProducts() {
+  let descProduct = await getProducts();
+  for (const product of descProduct) {
+    let renderedProduct = renderProduct(product);
+    section.appendChild(renderedProduct);
+  }
+}
+
 fetchData();
+
+console.log();
