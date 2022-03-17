bf097560987ebd3def65bc54d8c6e1a1e25a7925
Paul <paulvignon99@gmail.com>
2022-03-17 12:13:58 +0100

    function d'ajout au panier

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,42 @@
-import { stocker, recuperer, ajouterAuPanier } from "./store.js";
+import {
+  stocker,
+  recuperer,
+  ajouterAuPanier,
+  supprimerDuPanier,
+  modifierDuPanier,
+} from "./store.js";
 
 ajouterAuPanier;
+
+let template = document.querySelector("#cart__template");
+
+let section = document.querySelector("#cart__items");
+
+function renderProduct(data) {
+  let clone = document.importNode(template.content, true);
+  clone.querySelector("#cart__img").src = data.imageUrl;
+  clone.querySelector("#cart__img").alt = data.altTxt;
+  clone.querySelector("#cart__name").textContent = data.name;
+  clone.querySelector("#cart__price").textContent = `${data.price.toFixed(
+    2
+  )} €`;
+  clone.querySelector("#cart__color").textContent = data.color;
+  clone.querySelector("#cart__quantity").value = data.quantity;
+  clone.querySelector("#cart__delete").addEventListener("click", function () {
+    supprimerDuPanier(data);
+    renderAllProducts();
+    console.log("blabla", data);
+  });
+  return clone;
+}
+
+async function renderAllProducts() {
+  section.innerHTML = "";
+  let descProduct = recuperer();
+  for (const product of descProduct) {
+    let renderedProduct = renderProduct(product);
+    section.appendChild(renderedProduct);
+  }
+}
+
+renderAllProducts();
--- front/js/store.js
+++ front/js/store.js
@@ -1,5 +1,5 @@
 export function stocker(panier) {
-  const formJSON = JSON.stringify(formSelection);
+  const formJSON = JSON.stringify(panier);
   console.log(formJSON);
 
   localStorage.setItem("produit", formJSON);
@@ -29,6 +29,37 @@ export function ajouterAuPanier(produit) {
     // ajout : voir comment ajouter au panier
     panier.push(produit);
   }
+
+  //enregistrer dans le localStorage
+  stocker(panier);
+}
+
+export function supprimerDuPanier(produit) {
+  let panier = recuperer();
+  console.log(produit);
+
+  //verifier si il existe deja dans le panier
+  let index = panier.findIndex(
+    (item) => item._id === produit._id && produit.color === item.color
+  );
+
+  panier.splice(index, 1);
+
+  //enregistrer dans le localStorage
+  stocker(panier);
+}
+
+export function modifierDuPanier(produit) {
+  //recuperer contenue du localStorage
+  let panier = recuperer();
+  console.log(produit);
+
+  //verifier si il existe deja dans le panier
+  let index = panier.findIndex(
+    (item) => item._id === produit._id && produit.color === item.color
+  );
+
+  panier[index] = produit;
 
   //enregistrer dans le localStorage
   stocker(panier);
